2592d357d73a1ad38ce6ffe462b53e542e4fac06
Matteo <matteo.papini@polimi.it>
2020-01-29 14:37:14 +0100

    some fixes

--- safety_envs/envs/basic_reach.py
+++ safety_envs/envs/basic_reach.py
@@ -12,6 +12,7 @@ Wraps the basic safety-gym point-robot task reducing its state
 import gym
 import safety_envs.envs
 from safety_envs.utils import filter_ob
+import numpy as np
 
 
 class BasicReach(gym.Env):
@@ -19,6 +20,8 @@ class BasicReach(gym.Env):
     def __init__(self):
         self.wrapped = gym.make('PointReach-v0')
         self.mask = [1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0]  
+        self.observation_space = gym.spaces.Box(-np.inf, np.inf, (sum(self.mask),), dtype=np.float32) 
+        self.action_space = self.wrapped.action_space
     
     def step(self, action):
         ob, reward, done, info = self.wrapped.step(action)
@@ -30,7 +33,7 @@ class BasicReach(gym.Env):
         return filter_ob(ob, self.mask)
 
     def render(self, mode='human'):
-        return self.wrapped.render(mode=mode)
+        return self.wrapped.render(mode=mode, camera_id=0)
 
     def close(self):
         return self.wrapped.close()
