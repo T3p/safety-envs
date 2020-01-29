ab42a06b95ac1613d9ba82200a52e00a32fc2ba1
Matteo <matteo.papini@polimi.it>
2020-01-29 13:59:25 +0100

    some fixes

--- safety_envs/envs/basic_reach.py
+++ safety_envs/envs/basic_reach.py
@@ -16,21 +16,25 @@ from safety_envs.utils import filter_ob
 
 class BasicReach(gym.Env):
 
-  def __init__(self):
-    self.wrapped = gym.make('PointReach-v0')
-    self.mask = [1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0]  
+    def __init__(self):
+        self.wrapped = gym.make('PointReach-v0')
+        self.mask = [1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0]  
     
-  def step(self, action):
-    ob, reward, done, info = self.wrapped.step(action)
-    ob = filter_ob(ob, self.mask)
-    return ob, reward, done, info
+    def step(self, action):
+        ob, reward, done, info = self.wrapped.step(action)
+        ob = filter_ob(ob, self.mask)
+        return ob, reward, done, info
       
-  def reset(self):
-    ob = self.wrapped.reset()
-    return filter_ob(ob, self.mask)
+    def reset(self):
+        ob = self.wrapped.reset()
+        return filter_ob(ob, self.mask)
 
-  def render(self, mode='human'):
-    return self.wrapped.render(mode=mode)
+    def render(self, mode='human'):
+        return self.wrapped.render(mode=mode)
 
-  def close(self):
-    return self.wrapped.close()
+    def close(self):
+        return self.wrapped.close()
+
+    def seed(self, seed):
+        return self.wrapped.seed(seed)
+  
--- script/test_filter.py
+++ script/test_filter.py
@@ -9,21 +9,27 @@ Created on Wed Jan 29 13:37:38 2020
 import safety_envs
 import gym
 
+seed = 42
+
 env1 = gym.make('PointReach-v0')
 env2 = gym.make('BasicReach-v0')
 
 print('State dim of original task:')
-print('reset:', len(env1.reset()))
-print('step:', len(env1.step()))
+print('reset():', len(env1.reset()))
+print('step(1):', len(env1.step(1.)[0]))
 
 print('\nState dim of reduced task:')
-print('reset:', len(env2.reset()))
-print('step:', len(env2.step()))
-
-env1.seed(0)
-env2.seed(0)
+print('reset():', len(env2.reset()))
+print('step(1):', len(env2.step(1.)[0]))
 
 print()
-print(env1.reset(), '\n->\n', env2.reset())
+env1.seed(seed)
+print(env1.reset(), '\n->')
+env2.seed(seed)
+print(env2.reset())
+
 print()
-print(env1.step(1.), '\n->\n', env2.step(1.))
+env1.seed(seed)
+print(env1.step(1.)[0], '\n->')
+env2.seed(seed)
+print(env2.step(1.)[0])
